5faf6578a365b82d3f42f60edf64d1ce29bdbea4
Charco Hui <phui285@gmail.com>
2019-10-28 23:37:31 +1300

    fix format

---
+++ R/data-mpdata.R
@@ -0,0 +1,18 @@
+#' The needed data frames in my master project.
+#'
+#' The needed data frames in my master project.
+#'
+#' @format Data frame which contains information of species and ingradients.
+#' @examples
+#' data(myData_1)
+"myData_1"
+
+
+#' The needed data frames in my master project.
+#'
+#' The needed data frames in my master project.
+#'
+#' @format Data frame which contains information of species and ingradients.
+#' @examples
+#' data(myData_2)
+"myData_2"
--- R/treesplits.R
+++ R/treesplits.R
@@ -1,21 +1,61 @@
+#' A function to detect break points of species occurance in different ingradients.
+#'
+#' @param mydata A data frame, which contains species and ingradients information from different regions.
+#' @param species A list of charaters with names of the needed species.
+#' @param ingradients A list of charaters with names of the needed ingradients where to be shown in the tables.
+#' @param plot_title A logical value, which indicates showing plots with or without title.
+#' @param first_split A logical value, which indicates showing scatter plots with only the first split or showing all the splits. By default, first_split = FALSE, which means only the first splits will be shown.
+#' @param show.plots A choice of the showing plots. (show.plots = "splits") shows only the break points in the scatter plots. (show.plots = "dendrgram") shows only the tree dendrograms from the "rpart" models. (show.plots = "none") if no plot is needed. The default is (show.plots = "both"), which shows both the splits plots and the dendrograms.
+#' @return Returns a list of break points in different ingradients and for different species.
+#' @description
+#' This is a function of the package "cooccurExtra". The main idea of this function is detecting break points of species occurance in different ingradients with the information of species and ingradients from different regions. It provides two kinds of plots to show the breaks points in different ingradients of each specie. Also it provides an output of break points (splits) of each ingradient and for each specie.
+#' @author
+#' Yingjia Jot He
+#' @examples
+#' # create a vector of charaters with all the needed species' names
+#' Ten.Needed <- c("alpheus", "theora.lubrica", "aricidea",
+#'                 "paradonidae.other", "austrovenus.stutchburyi", "macomona.liliana",
+#'                 "maldanidae", "aonides", "prionospio.aucklandica", "orbiniidae")
+#'
+#' # create a vector of charaters with all the needed ingradients' names
+#' Ing.Needed <- c("Log.mud.1.", "SST")
+#'
+#' # ask for the data frame to be analized
+#' data(macrofauna)
+#'
+#' # the default with two kinds of plots
+#' mytest <- treesplits(macrofauna, species = Ten.Needed, ingradients = Ing.Needed)
+#'
+#' # show the output
+#' mytest
+#'
+#' # case with one specie that does not exist and show only the splits plots
+#' mytest <- treesplits(macrofauna, species = c(Ten.Needed, "fake.specie"),
+#'                      ingradients = Ing.Needed, show.plots = "splits")
+#'
+#' # case with one ingradient that does not exist and show only the dendrograms
+#' mytest <- treesplits(macrofauna, species = Ten.Needed,
+#'                      ingradients = c(Ing.Needed, "fake.specie"),
+#'                      show.plots = "dendrogram")
+
 # Function "treesplits"
 #####
 # Require Package "rpart" & "rattle"
 #####
-# A function to detect break points of species occurance in different ingradients 
+# A function to detect break points of species occurance in different ingradients
 #####
 treesplits = function(mydata, species, ingradients, first_split = FALSE,
-                      plot_title = TRUE, #method = "tree", 
-                      show.plots = c("both", "splits", 
+                      plot_title = TRUE, #method = "tree",
+                      show.plots = c("both", "splits",
                                      "dendrogram", "none")){
   #####*1* "mydata" is a data set with all the variables that we need
   #####*2* "methods" could be "tree" and some others, will update in the future
   #####*3* "species" can be any species that contain in data set
   #####*4* "ingradients" can be any species that contains in data set, no more than three types
-  #####*5* "show.plots" gives the choices of plots 
+  #####*5* "show.plots" gives the choices of plots
   #####*6* "plot_title" logical. indicates showing plots with or without title.
   #####*6* "first_split" logical. indicates showing scatter with only the first split.
-  
+
   ###########################################################################################
   #0. turn down warnings
   ###########################################################################################
@@ -31,24 +71,24 @@ treesplits = function(mydata, species, ingradients, first_split = FALSE,
   ## show warnings
   if(any(judge.species)){
     writeLines(c("Warning Message (1):",
-                 paste(ifelse(sum(judge.species) == 1, "Specie", "Species"), 
-                       paste(paste0("'", species[judge.species], "'"), 
+                 paste(ifelse(sum(judge.species) == 1, "Specie", "Species"),
+                       paste(paste0("'", species[judge.species], "'"),
                              collapse = ","),
                        ifelse(sum(judge.species) == 1, "is", "are"),
                        "not in the given data set, which",
                        ifelse(sum(judge.species) == 1, "has", "have"),
                        "been ignored.")))
-  } 
+  }
   if(any(judge.ingradients)){
     writeLines(c(paste("Warning Message", ifelse(any(judge.species), "(2):", "(1):")),
-                 paste(ifelse(sum(judge.ingradients) == 1, "Ingradient", "Ingradients"), 
-                       paste(paste0("'", ingradients[judge.ingradients], "'"), 
+                 paste(ifelse(sum(judge.ingradients) == 1, "Ingradient", "Ingradients"),
+                       paste(paste0("'", ingradients[judge.ingradients], "'"),
                              collapse = ","),
                        ifelse(sum(judge.ingradients) == 1, "is", "are"),
                        "not in the given data set, which",
                        ifelse(sum(judge.ingradients) == 1, "has", "have"),
                        "been ignored.")))
-  } 
+  }
   ## keep the rest
   keep.species = species[!judge.species]
   keep.ingradients = ingradients[!judge.ingradients]
@@ -71,7 +111,7 @@ treesplits = function(mydata, species, ingradients, first_split = FALSE,
   ###########################################################################################
   if(show.plots != "none"){
     op = par()
-    par(mfrow = mf) 
+    par(mfrow = mf)
   }
   ## get column names of given data set
   col.names = colnames(mydata)
@@ -89,7 +129,7 @@ treesplits = function(mydata, species, ingradients, first_split = FALSE,
       tempsplit = tempfit$splits[, 4]
       Splits[[n]][[m]] = tempsplit
       if(show.plots == "both" | show.plots == "splits"){
-        plot(x = mydata[, j], y = mydata[, i], type = "p", 
+        plot(x = mydata[, j], y = mydata[, i], type = "p",
              ylab = NameMacrofauna, xlab = keep.ingradients[n])
         if(plot_title){
           title(main = paste0(NameMacrofauna, " VS ", keep.ingradients[n]))
@@ -121,7 +161,7 @@ treesplits = function(mydata, species, ingradients, first_split = FALSE,
   ###########################################################################################
   #6. return values
   ###########################################################################################
-  # add names to the output list 
+  # add names to the output list
   names(Splits) = keep.ingradients
   for(i in 1:length(keep.ingradients)){
     names(Splits[[i]]) = keep.species
